ef29df4fe0ca0feffc89500272f4c33655b794a7
alompson <jp.alonso@usp.br>
2022-01-30 00:35:29 -0300

    added buttons start/pause and reset

--- script.js
+++ script.js
@@ -1,39 +1,126 @@
-let min = 25;
-let sec = 0;
-var t;
+let pomodoroTime = 25;
+let shortBreakTime = 5;
+let longBreakTime = 10;
+let sessionCounter = 1;
+let startPauseControl = 0 //to control when to start and when to pause
 
-let countdown = document.getElementById("countdown");
+
+let min = pomodoroTime;
+let sec = 0;
+let t;
+let mode = {
+    name: "",
+    time: pomodoroTime
+}; //can be "pomodoro", "short break" or "long break"
+
+let countdownDisplay = document.getElementById("countdown");
+let modeDisplay = document.getElementById("mode");
+let startPauseButton = document.getElementById("startpause");
+let configTimers = document.getElementById("configTimers");
+let pomodoroConfig = document.getElementById("pomodoroConfig");
+
+displayTime();
+//pomodoroConfig.innerHTML = (min > 9 ? (min):("0"+min));
 
 function tick() {
-    if (min === 0) {
-        endTimer()
-    }else{
-        if(sec === 0) {
-            min--;
-            sec = 59;
+    displayTime();
+    if(sec == 0) {
+        if(min == 0) {
+            clearInterval(t);
+            endTimer();
         }else{
-        sec--;
+            sec = 59;
+            min--;
         }
+    }else{
+        sec--;
     }
 }
 
 function endTimer(){
-
+    if(sessionCounter != 0 && sessionCounter % 4 == 0) {
+    //every 4th break session, there is a long pause instead of a short pause
+    startLongBreak();
+    }else {
+        if(mode == "Pomodoro") {
+        //a working session just ended
+            startShortBreak();
+        }else{
+        //a short break session just ended, which means a complete session ended
+            sessionCounter++;
+            startPomodoro();
+        }
+    }
 }
 
-function displayTime () {
-    countdown.innerHTML = (min > 9 ? (min):("0"+min))
+function displayTime() {
+    countdownDisplay.innerHTML = (min > 9 ? (min):("0"+min))
     + ":" + (sec > 9 ? (sec):("0"+sec));
 }
 
-function subtractSecond() {
+function countdown() {
+    min = mode.time;
+    t = setInterval(tick, 1000);
+}
+
+function startPomodoro() {
+    mode.name = "Working Session (be focused!)";
+    modeDisplay.innerHTML = mode.name;
+    mode.time = pomodoroTime;
+    countdown();
+}
+
+function startShortBreak() {
+    mode.name = "Short Break (relax my man)";
+    modeDisplay.innerHTML = mode.name;
+    mode.time = shortBreakTime;
+    countdown();
+}
+
+function startLongBreak() {
+    mode.name = "Long Break (ultimate relax)";
+    modeDisplay.innerHTML = mode.name;
+    mode.time = longBreakTime;
+    countdown();
+}
+
+function startAndPause(){
+    if (startPauseControl == 0) {
+        //first time button is pressed. Set variable to 1 and start Pomodoro
+        startPauseControl = 1;
+        startPauseButton.innerHTML = "Pause";
+        startPomodoro();
+
+    } else if(startPauseControl == 1) {
+        //stop pomodoro
+        startPauseControl = 2;
+        startPauseButton.innerHTML = "Start";
+        clearInterval(t);
+
+    } else if (startPauseControl == 2) {
+        //resume pomodoro
+        startPauseControl = 1;
+        startPauseButton.innerHTML = "Pause";
+        countdown();
+    }
+}
+
+function reset() {
+    clearInterval(t);
+    startPauseControl = 0;
+    startPauseButton.innerHTML = "Start";
+    sec = 0;
+    min = mode.time;
     displayTime();
-    tick();
-    timer();
 }
 
-function timer() {
-    t = setTimeout(subtractSecond, 1000);
+/*
+function subtractPomodoroMin() {
+    if(pomodoroTime > 0){
+        pomodoroTime--;
+        min--;
+    }
+    displayTime();
+    pomodoroConfig.innerHTML = (min > 9 ? (min):("0"+min));
 }
-
-timer();
+*/
